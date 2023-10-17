45c75683ea21e7c8e2d8d2508913f7f0271848a8
NurAzzahraMaudina <146052974+NurAzzahraMaudina@users.noreply.github.com>
2023-10-17 20:29:38 +0800

    Create Static_field.dart

---
+++ Static_field.dart
@@ -0,0 +1,10 @@
+class Application{
+  static final String author = "Nur Azzahra Maudina";
+  static final String name = "Tutor Pemprograman Dart";
+  
+}
+
+void main(){
+  print(Application.name);
+  print(Application.author);
+}
